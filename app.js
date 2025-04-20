5cfd261a047dc3205ea5199cf5e9325a5142ab62
wanning-code <198075446+wanning-code@users.noreply.github.com>
2025-04-20 12:35:45 +0200

    commtents with a better solution to avoid repetitive code

--- app.js
+++ app.js
@@ -18,20 +18,35 @@ let i = 10
 // }
 
 // My Solution
+// const para = document.createElement('p'); and output.appendChild(para);
+// an be put before and after the if else if statements to avoid repetitive code
 for (let i = 10; i >= 0; i--) {
-
+    // const para = document.createElement('p');
     if(i === 10) {
+        // Can be replaced and put before the if statement to avoid repetitive code
         const para = document.createElement('p');
-        para.textContent += `Countdown ${i}`; 
+
+        para.textContent += `Countdown ${i}`;
+
+        // Can be replaced and put after the if statement to avoid repetitive code
         output.appendChild(para);
     } else if (i === 0) {
+        // Can be replaced and put before the if statement to avoid repetitive code
         const para = document.createElement('p');
+
         para.textContent += "Blast off!"; 
+
+        // Can be replaced and put after the if statement to avoid repetitive code
         output.appendChild(para);
         
     } else {
+        // Can be replaced and put before the if statement to avoid repetitive code
         const para = document.createElement('p');
+
         para.textContent += `${i}`; 
+
+        // Can be replaced and put after the if statement to avoid repetitive code
         output.appendChild(para);
     }
+    // output.appendChild(para);
 }
